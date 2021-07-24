3ae200da828b888371d9be9ecaa4ee8f734b0057
Lovecraft5 <Eduardomqesco@gmail.com>
2021-07-24 11:49:25 +0200

    Plan del programa

---
+++ main.py
@@ -0,0 +1,12 @@
+################################################################################
+##
+## JUEGO DE AJEDREZ
+##
+## Autor: Eduardo Molina
+##
+## Este programa va a imitar un videojuego de ajedrez
+## Tablero -> casillas 64 -> cordenadas y sus estados de ocupación
+## Piezas->estado->valor->cordenadas->movimiento/comer/morir->tipo de pieza
+## Jugador->color->tiempo->puntos
+## Main Prgrama del juego
+################################################################################
